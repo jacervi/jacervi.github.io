d975c40fdfc4ba5a4841e52202acf6ea4aa2529a
jacervi <157400733+jacervi@users.noreply.github.com>
2025-01-06 12:28:58 +0100

    MiWEB

---
+++ gestionJSON.js
@@ -0,0 +1,26 @@
+//Lectura de ficheros JSON
+function leeJSON() {
+  const fs = require("fs");
+  let data = fs.readFileSync("documentos.json");
+  //console.log(data)
+  let documento = JSON.parse(data);
+  return (documento)
+}
+//MOdificación de archivos JSON
+function modificaJSON(tema1, nombre) {
+  //leeJSON();
+
+  const fs = require("fs");
+  let data = fs.readFileSync("documentos.json");
+  //console.log(data)
+  let documento = JSON.parse(data);
+  documento.documentos[3].tema = tema1
+  documento.documentos[3].titulo = nombre;
+  fs.writeFileSync("documentos1.json", JSON.stringify(documento));
+  console.log(documento);
+  
+  //Número de elementos en JSON
+  console.log(documento.documentos.length)
+}
+
+modificaJSON("C++", "Esta es otra prueba de título");
